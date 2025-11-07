46cc7714b94218fdce16b40c7b9622dcc5ea8c75
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-07 16:29:47 +0100

    feat:Aggiunta dei numeri che compaiono e scompaiono

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Genera numeri casuali
 const RandomNum = [];
 
 for (let i = 0; i < 5; i++) {
@@ -24,4 +25,27 @@ for (let i = 0; i < 5; i++) {
     RandomNum.push(newNum);
 }
 console.log(RandomNum);
+const risRandomNum = RandomNum.join(" ");
 
+//Output in pagina
+let outputRandomNum = "";
+const randomNumElem = document.querySelector(".randomnum");
+
+for (let i = 0; i < RandomNum.length; i++) {
+    outputRandomNum += "<span>" + RandomNum[i] + "</span>";
+}
+randomNumElem.innerHTML = outputRandomNum;
+
+
+
+let count = 5;
+const intervalId = setInterval(function () {
+    if (count === 0) {
+        randomNumElem.innerHTML = "";
+        clearInterval(intervalId);
+    } else {
+        randomNumElem.innerHTML = outputRandomNum;
+
+    }
+    count--;
+}, 1000);
